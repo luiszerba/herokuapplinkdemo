8413d3066820e97a0e917043373bb4c7fbd09e69
Luis Zerba <lcorrea@lcorrea-ltmb9nn.internal.salesforce.com>
2025-04-24 16:20:28 -0300

    changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
+app.get('*', (_, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
