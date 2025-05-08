0d0d3c1fb5f79f4ec3ed13d0ed02d83d8d2fce94
Luis Zerba <lcorrea@lcorrea-ltmb9nn.internal.salesforce.com>
2025-05-08 11:55:51 -0300

    changes

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -61,7 +61,7 @@ export default function RestaurantCard({ place, onSelect, onRequireLogin, isFavo
         }
       });*/
 
-      SalesforceInteractions.sendEvent({
+      /*SalesforceInteractions.sendEvent({
         interaction: {
           name: "RestaurantInteraction",
           //eventType: "restaurantInteraction",
@@ -71,6 +71,19 @@ export default function RestaurantCard({ place, onSelect, onRequireLogin, isFavo
           restaurantInteraction_favoritado__c: isAdding.toString() ,
           restaurantInteraction_locationId__c: place.id ,
           restaurantInteraction_restaurantName__c: place.nome || place?.detalhes_json?.overview?.name || 'Desconhecido' 
+        }
+      });*/
+
+      SalesforceInteractions.sendEvent({
+        interaction: {
+          name: "RestaurantInteraction",
+          //eventType: "restaurantInteraction",
+          //category: "restaurants",
+          customeremail__c: usuario.email ,
+          cpf__c: usuario.cpf ,
+          favoritado__c: isAdding.toString() ,
+          locationId__c: place.id ,
+          restaurantName__c: place.nome || place?.detalhes_json?.overview?.name || 'Desconhecido' 
         }
       });
 
